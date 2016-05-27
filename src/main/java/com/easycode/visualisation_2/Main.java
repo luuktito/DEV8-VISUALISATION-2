fd386539d7e9c1cb123f54024b271aa2a749f1b1
luuktito <luuktito@gmail.com>
2016-05-27 17:34:45 +0200

    Finished 2B, scatterplot works with scaling axis values. All nodes have a different color based on the category.

--- src/main/java/com/easycode/visualisation_2/Main.java
+++ src/main/java/com/easycode/visualisation_2/Main.java
@@ -5,9 +5,6 @@
  */
 package com.easycode.visualisation_2;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 import java.util.ArrayList;
@@ -15,7 +12,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import processing.core.PApplet;
 import static processing.core.PApplet.map;
-import static processing.core.PApplet.println;
 import static processing.core.PConstants.CENTER;
 import processing.core.PImage;
 
@@ -28,10 +24,10 @@ public class Main extends PApplet{
     PImage photo;
     private static ArrayList<PlotData> scatterPlotDataList = new ArrayList<>();
     private static TextReader newTextReader = new TextReader();
-//    private static ArrayList<Earthquake> earthquakeList = new ArrayList<>();
-//    private static final JSONtoEarthquake newJsontoEarthquake = new JSONtoEarthquake();
-//    JsonObject earthquakes;
-//    JsonArray values;
+    float minValueX;
+    float minValueY; 
+    float maxValueX; 
+    float maxValueY; 
 
     public static void main(String[] args) {
         try {
@@ -51,19 +47,16 @@ public class Main extends PApplet{
     public void setup(){
         noLoop();
         background(255,255,255);
-        //photo = loadImage("IcelandMap.png", "png");
-        //image(photo, 100,100,1000,812);
-        
-        //drawScatterPlot();
-        //drawLegend();
-        //drawEarthquakes();
     }
 
     @Override
     public void draw() {
+        minValueX = calculateMin("EIG1").getEIG1();
+        minValueY = calculateMin("EIG2").getEIG2();
+        maxValueX = calculateMax("EIG1").getEIG1();
+        maxValueY = calculateMax("EIG2").getEIG2();
         drawLegend();
         drawScatterPlot();
-        //setGradient(900, 950, 200, 10);
     }
     
     public PlotData calculateMin(String type) {
@@ -97,62 +90,70 @@ public class Main extends PApplet{
     }
     
     public void drawScatterPlot() {
+        strokeWeight((float) 0.5);
         for (PlotData SP : scatterPlotDataList) {
-            float maxValueX = calculateMax("EIG1").getEIG1();
-            float maxValueY = calculateMax("EIG2").getEIG2();
-            
-            float xPos = map(SP.getEIG1(), 0, maxValueX, 100, 600);
-            float yPos = map(SP.getEIG2(), 0, maxValueY, 600, 100);
-            
-            ellipse(xPos, yPos, 5, 5);
-            
-//            int c = color(255, (255 - ((EQ.getSize()+1)*85)), 0);
-//            fill(c);
-//            ellipse(((27+EQ.getLongitude())*74),(((68-EQ.getLatitude())*180)), (EQ.getDepth() * 2), (EQ.getDepth() * 2));
-//            
-//            println("Latitude: " + EQ.getLatitude() + ", Longitude: " + EQ.getLongitude() + ", Depth: " + EQ.getDepth() + ", Size: " + EQ.getSize());
+            float xPos = map(SP.getEIG1(), minValueX, maxValueX, 100, 600);
+            float yPos = map(SP.getEIG2(), minValueY, maxValueY, 600, 100);
+            int color = 0;
+            switch(SP.getCAT()) {
+                case 1 :
+                    color = color(255, 0, 0);
+                    break;
+                case 2 :
+                    color = color(255, 255, 0);
+                    break;
+                case 3 :
+                    color = color(0, 255, 0);
+                    break;
+                case 4 :
+                    color = color(0, 0, 255);
+                    break;
+                default:
+            }
+            fill(color);
+            ellipse(xPos, yPos, 8, 8);
         }
     }
 
     public void drawLegend() {
+        float differenceMinMaxX = maxValueX - minValueX;
+        float differenceMinMaxY = maxValueY - minValueY;
+        
+        textAlign(CENTER);
         strokeWeight((float) 1.5);
         fill(255,255,255);
         stroke(5);
         rect(90, 90, 520, 520);
         fill(0,0,0);
         stroke(1);
-//        stroke(4);
-//        textAlign(CENTER);
-//        textSize(14);
-//        fill(1);
-//        text("Scale of Richter", 955, 940);
-//        textSize(12);
-//        text("-1              0               1               2", 1000, 975);
-//        textSize(20);
-//        text("Earthquakes Iceland from " + earthquakeList.get(earthquakeList.size() -1).getTimestamp()+ " to " + earthquakeList.get(0).getTimestamp(), 600, 50);
-//        textSize(14);
-//        text("From vedur.is", 600, 80);
-//
-//        strokeWeight((float) 1.0);
-//        fill(255,255,255);
-//        rect(750, 950, 50, 10);
-//        fill(0,0,0);
-//        rect(800, 950, 50, 10);
-//        line(750, 950, 750, 965);
-//        line(850, 950, 850, 965);
-//
-//        textSize(12);
-//        text("0                       25 km", 815, 975);
-//        textSize(14);
-//        text("Depth in KM", 791, 940);
+        textSize(20);
+        text("Scatterplot of EIG2 over EIG1", width / 2, height / 10);
+        textSize(16);
+        text("EIG1", width / 2, 660);
+        text("EIG2", 23, height / 2);
+        textSize(12);
+        
+        text("CAT =  1:          2:", 540, 660);
+        text("           3:          4:", 540, 680);
+        fill(255, 0, 0);
+        ellipse(550, 655, 8, 8);
+        fill(255, 255, 0);
+        ellipse(600, 655, 8, 8);
+        fill(0, 255, 0);
+        ellipse(550, 675, 8, 8);
+        fill(0, 0, 255);
+        ellipse(600, 675, 8, 8);
+        
+        fill(0, 0, 0);
+        for (int i = 0; i < 510; i = i+50) {
+            line(100 + i, 610, 100 + i, 615);
+            text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+        }
+        
+        textAlign(RIGHT);
+        for (int i = 0; i < 510; i = i+50) {
+            line(90, 100 + i, 85, 100 + i);
+            text("" + (int) (maxValueY - (differenceMinMaxY * ((float)i/500))), 70, 100 + i);
+        }
     }
-//    
-//    public void setGradient(int x, int y, float w, float h) {
-//        for (int i = x; i <= x+w; i+=1) {
-//          float inter = map(i, x, x+w, 0, 1);
-//          int c = lerpColor(color(255, 255, 0), color(255, 0, 0), inter);
-//          stroke(c);
-//          line(i, y, i, y+h);
-//        }
-//    }
 }
