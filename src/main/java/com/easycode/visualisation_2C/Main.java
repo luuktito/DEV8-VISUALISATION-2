3c7342abdb5a2d0ff82b9167f77f73cb9baa308a
luuktito <luuktito@gmail.com>
2016-05-28 19:34:02 +0200

    Finished Assignment 2C (Added axis values to axis markers)

--- src/main/java/com/easycode/visualisation_2C/Main.java
+++ src/main/java/com/easycode/visualisation_2C/Main.java
@@ -6,6 +6,7 @@
 package com.easycode.visualisation_2C;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +26,6 @@ public class Main extends PApplet{
     PImage photo;
     private static ArrayList<PlotData> scatterPlotDataList = new ArrayList<>();
     private static TextReader newTextReader = new TextReader();
-//    AxisValues axisValuesLFTD;
-//    AxisValues axisValuesANA;
-//    AxisValues axisValuesDEV;
-//    AxisValues axisValuesPRJ;
-//    AxisValues axisValuesSKL;
     List<List<Float>> plotValues = new ArrayList<>();
     ArrayList<AxisValues> axisValuesAll = new ArrayList<>();
     
@@ -92,18 +88,11 @@ public class Main extends PApplet{
         axisValuesAll.add(new AxisValues(minPlotValueDEV.getDEV(), maxPlotValueDEV.getDEV()));
         axisValuesAll.add(new AxisValues(minPlotValuePRJ.getPRJ(), maxPlotValuePRJ.getPRJ()));
         axisValuesAll.add(new AxisValues(minPlotValueSKL.getSKL(), maxPlotValueSKL.getSKL()));
-
-//        axisValuesLFTD = new AxisValues(minPlotValueLftd.getLftd(), maxPlotValueLftd.getLftd());
-//        axisValuesANA = new AxisValues(minPlotValueANA.getANA(), maxPlotValueANA.getANA());
-//        axisValuesDEV = new AxisValues(minPlotValueDEV.getDEV(), maxPlotValueDEV.getDEV());
-//        axisValuesPRJ = new AxisValues(minPlotValuePRJ.getPRJ(), maxPlotValuePRJ.getPRJ());
-//        axisValuesSKL = new AxisValues(minPlotValueSKL.getSKL(), maxPlotValueSKL.getSKL());
     }
 
     public void calculateMapCoordinates() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
             plotValues.add(new ArrayList<Float>());
-        }
         
         for (PlotData SP : scatterPlotDataList) {
             plotValues.get(0).add(map(SP.getLftd(), axisValuesAll.get(0).getMinValue(), axisValuesAll.get(0).getMaxValue(), 100, 250));
@@ -114,7 +103,10 @@ public class Main extends PApplet{
         }
     }
     public void drawScatterPlot() {
-        rectMode(CENTER);      
+        rectMode(CENTER);   
+        textSize(12);
+        DecimalFormat df = new DecimalFormat("#.#");
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 noFill();
@@ -122,39 +114,48 @@ public class Main extends PApplet{
                 rect((float) (170 * i) + 175, (float) (170 * j) + 175, 160, 160);
                 fill(0);
                 strokeWeight((float) 0.8);
-                
+                 
                 if ((i==0 || i==4 )) {
+                    if (axisValuesAll.get(j).getMaxValue() >= 10)
+                        df.setMaximumFractionDigits(0);
+                    else 
+                        df.setMaximumFractionDigits(1);
+                    
                     if (j%2==0) {
-                        for (int l = 0; l < 150; l = l+25) {
-                            line(940, 110 + l + (j * 170), 935, 110 + l + (j * 170));
-                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        for (int l = 0; l < 180; l = l+30) {
+                            line(940, 100 + l + (j * 170), 935, 100 + l + (j * 170));
+                            text(df.format((axisValuesAll.get(j).getMaxValue() - ((axisValuesAll.get(j).getMaxValue() - axisValuesAll.get(j).getMinValue()) * ((float) l/150)))), 945, 105 + l + j * 170);
                         }
                     }
                     else {
-                        for (int l = 0; l < 150; l = l+25) {
-                            line(95, 110 + l + (j * 170), 90, 110 + l + (j * 170));
-                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        for (int l = 0; l < 180; l = l+30) {
+                            line(95, 100 + l + (j * 170), 90, 100 + l + (j * 170));
+                            text(df.format((axisValuesAll.get(j).getMaxValue() - ((axisValuesAll.get(j).getMaxValue() - axisValuesAll.get(j).getMinValue()) * ((float) l/150)))), 70, 105 + l + j * 170);
                         }
                     }
                 }
-                
                 if ((j==0 || j==4 )) {
-                    if (i%2==0) {
-                        for (int l = 0; l < 150; l = l+25) {
-                            line(110 + l + (i * 170), 95, 110 + l + (i * 170), 90);
-                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                    if (axisValuesAll.get(i).getMaxValue() >= 10)
+                        df.setMaximumFractionDigits(0);
+                    else 
+                        df.setMaximumFractionDigits(1);
+                    
+                    if (i%2==0) {                       
+                        for (int l = 0; l < 180; l = l+30) {
+                            line(100 + l + (i * 170), 95, 100 + l + (i * 170), 90);
+                            text(df.format((axisValuesAll.get(i).getMinValue() + ((axisValuesAll.get(i).getMaxValue() - axisValuesAll.get(i).getMinValue()) * ((float) l/150)) * 0.7)), 92 + l + i * 170, 85);
                         }
                     }
                     else {
-                        for (int l = 0; l < 150; l = l+25) {
-                            line(110 + l + (i * 170), 940, 110 + l + (i * 170), 935);
-                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        for (int l = 0; l < 180; l = l+30) {
+                            line(100 + l + (i * 170), 940, 100 + l + (i * 170), 935);
+                            text(df.format((axisValuesAll.get(i).getMinValue() + ((axisValuesAll.get(i).getMaxValue() - axisValuesAll.get(i).getMinValue()) * ((float) l/150)))), 92 + l + i * 170, 955);
                         }
                     }
                 }
-
                 
                 strokeWeight((float) 0.1);
+                
                 if (i != j) {
                     for (int k = 0; k < plotValues.get(0).size(); k++) {
                         ellipse(plotValues.get(i).get(k) + (float) (170 * i), (150 - plotValues.get(j).get(k)) + (float) (170 * j) + 200, 3, 3);    
