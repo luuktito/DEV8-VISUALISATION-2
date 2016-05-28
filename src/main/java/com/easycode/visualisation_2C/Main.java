6ee61cd6f625858442b51eafbaced472bf85cac6
luuktito <luuktito@gmail.com>
2016-05-28 11:05:53 +0200

    Added axis markers to the scatter plot TODO Add axis values on the markers

--- src/main/java/com/easycode/visualisation_2C/Main.java
+++ src/main/java/com/easycode/visualisation_2C/Main.java
@@ -25,13 +25,13 @@ public class Main extends PApplet{
     PImage photo;
     private static ArrayList<PlotData> scatterPlotDataList = new ArrayList<>();
     private static TextReader newTextReader = new TextReader();
-    AxisValues axisValuesLFTD;
-    AxisValues axisValuesANA;
-    AxisValues axisValuesDEV;
-    AxisValues axisValuesPRJ;
-    AxisValues axisValuesSKL;
-    List<List<Float>> plotValues = new ArrayList<List<Float>>();
-
+//    AxisValues axisValuesLFTD;
+//    AxisValues axisValuesANA;
+//    AxisValues axisValuesDEV;
+//    AxisValues axisValuesPRJ;
+//    AxisValues axisValuesSKL;
+    List<List<Float>> plotValues = new ArrayList<>();
+    ArrayList<AxisValues> axisValuesAll = new ArrayList<>();
     
     public static void main(String[] args) {
         try {
@@ -87,11 +87,17 @@ public class Main extends PApplet{
             maxPlotValueSKL = (maxPlotValueSKL == null || SP.getSKL() > maxPlotValueSKL.getSKL()) ? SP:maxPlotValueSKL;
         }
         
-        axisValuesLFTD = new AxisValues(minPlotValueLftd.getLftd(), maxPlotValueLftd.getLftd());
-        axisValuesANA = new AxisValues(minPlotValueANA.getANA(), maxPlotValueANA.getANA());
-        axisValuesDEV = new AxisValues(minPlotValueDEV.getDEV(), maxPlotValueDEV.getDEV());
-        axisValuesPRJ = new AxisValues(minPlotValuePRJ.getPRJ(), maxPlotValuePRJ.getPRJ());
-        axisValuesSKL = new AxisValues(minPlotValueSKL.getSKL(), maxPlotValueSKL.getSKL());
+        axisValuesAll.add(new AxisValues(minPlotValueLftd.getLftd(), maxPlotValueLftd.getLftd()));
+        axisValuesAll.add(new AxisValues(minPlotValueANA.getANA(), maxPlotValueANA.getANA()));
+        axisValuesAll.add(new AxisValues(minPlotValueDEV.getDEV(), maxPlotValueDEV.getDEV()));
+        axisValuesAll.add(new AxisValues(minPlotValuePRJ.getPRJ(), maxPlotValuePRJ.getPRJ()));
+        axisValuesAll.add(new AxisValues(minPlotValueSKL.getSKL(), maxPlotValueSKL.getSKL()));
+
+//        axisValuesLFTD = new AxisValues(minPlotValueLftd.getLftd(), maxPlotValueLftd.getLftd());
+//        axisValuesANA = new AxisValues(minPlotValueANA.getANA(), maxPlotValueANA.getANA());
+//        axisValuesDEV = new AxisValues(minPlotValueDEV.getDEV(), maxPlotValueDEV.getDEV());
+//        axisValuesPRJ = new AxisValues(minPlotValuePRJ.getPRJ(), maxPlotValuePRJ.getPRJ());
+//        axisValuesSKL = new AxisValues(minPlotValueSKL.getSKL(), maxPlotValueSKL.getSKL());
     }
 
     public void calculateMapCoordinates() {
@@ -100,11 +106,11 @@ public class Main extends PApplet{
         }
         
         for (PlotData SP : scatterPlotDataList) {
-            plotValues.get(0).add(map(SP.getLftd(), axisValuesLFTD.getMinValue(), axisValuesLFTD.getMaxValue(), 100, 250));
-            plotValues.get(1).add(map(SP.getANA(), axisValuesANA.getMinValue(), axisValuesANA.getMaxValue(), 100, 250));
-            plotValues.get(2).add(map(SP.getDEV(), axisValuesDEV.getMinValue(), axisValuesDEV.getMaxValue(), 100, 250));
-            plotValues.get(3).add(map(SP.getPRJ(), axisValuesPRJ.getMinValue(), axisValuesPRJ.getMaxValue(), 100, 250));
-            plotValues.get(4).add(map(SP.getSKL(), axisValuesSKL.getMinValue(), axisValuesSKL.getMaxValue(), 100, 250));
+            plotValues.get(0).add(map(SP.getLftd(), axisValuesAll.get(0).getMinValue(), axisValuesAll.get(0).getMaxValue(), 100, 250));
+            plotValues.get(1).add(map(SP.getANA(),  axisValuesAll.get(1).getMinValue(), axisValuesAll.get(1).getMaxValue(), 100, 250));
+            plotValues.get(2).add(map(SP.getDEV(),  axisValuesAll.get(2).getMinValue(), axisValuesAll.get(2).getMaxValue(), 100, 250));
+            plotValues.get(3).add(map(SP.getPRJ(),  axisValuesAll.get(3).getMinValue(), axisValuesAll.get(3).getMaxValue(), 100, 250));
+            plotValues.get(4).add(map(SP.getSKL(),  axisValuesAll.get(4).getMinValue(), axisValuesAll.get(4).getMaxValue(), 100, 250));
         }
     }
     public void drawScatterPlot() {
@@ -114,8 +120,41 @@ public class Main extends PApplet{
                 noFill();
                 strokeWeight((float) 1.0);
                 rect((float) (170 * i) + 175, (float) (170 * j) + 175, 160, 160);
-                strokeWeight((float) 0.1);
                 fill(0);
+                strokeWeight((float) 0.8);
+                
+                if ((i==0 || i==4 )) {
+                    if (j%2==0) {
+                        for (int l = 0; l < 150; l = l+25) {
+                            line(940, 110 + l + (j * 170), 935, 110 + l + (j * 170));
+                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        }
+                    }
+                    else {
+                        for (int l = 0; l < 150; l = l+25) {
+                            line(95, 110 + l + (j * 170), 90, 110 + l + (j * 170));
+                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        }
+                    }
+                }
+                
+                if ((j==0 || j==4 )) {
+                    if (i%2==0) {
+                        for (int l = 0; l < 150; l = l+25) {
+                            line(110 + l + (i * 170), 95, 110 + l + (i * 170), 90);
+                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        }
+                    }
+                    else {
+                        for (int l = 0; l < 150; l = l+25) {
+                            line(110 + l + (i * 170), 940, 110 + l + (i * 170), 935);
+                            //text("" + (int) (minValueX + (differenceMinMaxX * ((float)i/500))), 100 + i, 630);
+                        }
+                    }
+                }
+
+                
+                strokeWeight((float) 0.1);
                 if (i != j) {
                     for (int k = 0; k < plotValues.get(0).size(); k++) {
                         ellipse(plotValues.get(i).get(k) + (float) (170 * i), (150 - plotValues.get(j).get(k)) + (float) (170 * j) + 200, 3, 3);    
@@ -128,10 +167,12 @@ public class Main extends PApplet{
     public void drawLegend() {
         textSize(24);
         textAlign(CENTER);
-        text("lftd", 175, 185);
+        text("AGE", 175, 185);
         text("ANA", 345, 355);
         text("DEV", 515, 525);
         text("PRJ", 685, 695);
         text("SKL", 855, 865);
+        textSize(20);
+        text("Matrix Plot of Age and grades for ANA, DEV, PRJ and SKL", width / 2, 50);
     }
 }
